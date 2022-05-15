423339cd1d1468e50f4f2b0fcf4f70f04907892d
Suwanshree Acharya <suwanshree@gmail.com>
2022-05-15 22:42:36 +0100

    Backend routes for reviews

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,4 @@
 from .listing_routes import listing_routes
-# from .review_routes import review_routes
+from .review_routes import review_routes
 from .auth_routes import auth_routes
 from .user_routes import user_routes

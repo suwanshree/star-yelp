74f16c0a196cdb9985d39556b4268c640835ad72
Suwanshree Acharya <suwanshree@gmail.com>
2022-05-21 00:27:48 +0100

    Final styling

--- react-app/src/components/LoginModal/LoginForm.js
+++ react-app/src/components/LoginModal/LoginForm.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory, Redirect } from "react-router-dom";
 import { login } from "../../store/session";
 import DemoButton from "../auth/DemoButton";
+import loginGif from "../../images/logingif.gif";
 
 function LoginForm() {
   const dispatch = useDispatch();
@@ -33,45 +34,52 @@ function LoginForm() {
 
   return (
     <div className="auth-form-container">
-      <form className="auth-form" onSubmit={onLogin}>
-        <h3>Login</h3>
-        <ul className="errors-div">
-          {errors.map((error, ind) => (
-            <li key={ind}>{error}</li>
-          ))}
-        </ul>
-        <div className="user-box">
-          <label className="auth-label" htmlFor="email">
-            Email *
-          </label>
-          <input
-            className="auth-input"
-            name="email"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={updateEmail}
-          />
+      <div className="auth-form-left">
+        <form className="auth-form" onSubmit={onLogin}>
+          <h3>Login</h3>
+          <ul className="errors-div">
+            {errors.map((error, ind) => (
+              <li key={ind}>{error}</li>
+            ))}
+          </ul>
+          <div className="user-box">
+            <label className="auth-label" htmlFor="email">
+              Email *
+            </label>
+            <input
+              className="auth-input"
+              name="email"
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div className="user-box">
+            <label className="auth-label" htmlFor="password">
+              Password *
+            </label>
+            <input
+              className="auth-input"
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+          <button id="auth-submit" type="submit">
+            Login
+          </button>
+        </form>
+        <div id="form-demo">
+          <DemoButton />
         </div>
-        <div className="user-box">
-          <label className="auth-label" htmlFor="password">
-            Password *
-          </label>
-          <input
-            className="auth-input"
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={updatePassword}
-          />
-        </div>
-        <button id="auth-submit" type="submit">
-          Login
-        </button>
-      </form>
-      <div id="form-demo">
-        <DemoButton />
+      </div>
+      <div className="auth-form-right">
+        <h3>Welcome back citizen</h3>
+        <img src={loginGif} className="login-gif" alt="Login Gif"></img>
+        <h3>Fuel up and get ready to jump!</h3>
       </div>
     </div>
   );

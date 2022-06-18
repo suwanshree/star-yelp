f28dc2825985261119620e8d27a931e7b9ce2abe
Suwanshree Acharya <suwanshree@gmail.com>
2022-06-18 14:45:36 +0100

    Checkbox state updates

--- react-app/src/components/Listings.js
+++ react-app/src/components/Listings.js
@@ -13,6 +13,13 @@ function Listings() {
   const sessionUser = useSelector((state) => state.session.user);
   const listingsObj = useSelector((state) => state.listings);
   const { setCurrentSearch } = useContext(SearchContext);
+  const [ allChecked, setAllChecked ] = useState(true);
+  const [ restChecked, setRestChecked ] = useState(false);
+  const [ barChecked, setBarChecked ] = useState(false);
+  const [ equipChecked, setEquipChecked ] = useState(false);
+  const [ retailChecked, setRetailChecked ] = useState(false);
+  const [ medicalChecked, setMedicalChecked ] = useState(false);
+  const [ miscChecked, setMiscChecked ] = useState(false);
   const listings = Object.values(listingsObj);
   const [title, setTitle] = useState("Star Yelp | Listings");
 
@@ -28,6 +35,64 @@ function Listings() {
   const onSearch = async (e) => {
     e.preventDefault();
     history.push("/results");
+  };
+
+  const handleAllChange = () => {
+    setAllChecked(true);
+    setRestChecked(false);
+    setBarChecked(false);
+    setEquipChecked(false);
+    setRetailChecked(false);
+    setMedicalChecked(false);
+    setMiscChecked(false);
+  };
+
+  const handleRestChange = (boolean) => {
+    if (boolean === true) setRestChecked(false);
+    else if (boolean === false) {
+      setRestChecked(true);
+      setAllChecked(false)
+    }
+  };
+
+  const handleBarChange = (boolean) => {
+    if (boolean === true) setBarChecked(false);
+    else if (boolean === false) {
+      setBarChecked(true);
+      setAllChecked(false)
+    }
+  };
+
+  const handleEquipChange = (boolean) => {
+    if (boolean === true) setEquipChecked(false);
+    else if (boolean === false) {
+      setEquipChecked(true);
+      setAllChecked(false)
+    }
+  };
+
+  const handleRetailChange = (boolean) => {
+    if (boolean === true) setRetailChecked(false);
+    else if (boolean === false) {
+      setRetailChecked(true);
+      setAllChecked(false)
+    }
+  };
+
+  const handleMedicalChange = (boolean) => {
+    if (boolean === true) setMedicalChecked(false);
+    else if (boolean === false) {
+      setMedicalChecked(true);
+      setAllChecked(false)
+    }
+  };
+
+  const handleMiscChange = (boolean) => {
+    if (boolean === true) setMiscChecked(false);
+    else if (boolean === false) {
+      setMiscChecked(true);
+      setAllChecked(false)
+    }
   };
 
   return (
@@ -73,31 +138,31 @@ function Listings() {
         <ul className="nav">
           <li className="top">CATEGORIES</li>
           <li className="check-box">
-            <input type="checkbox" />
+            <input type="checkbox" checked={allChecked} onChange={() => handleAllChange()}/>
             <label>All</label>
           </li>
           <li className="check-box">
-            <input type="checkbox" />
+            <input type="checkbox" checked={restChecked} onChange={() => handleRestChange(restChecked)}/>
             <label>Restaurants</label>
           </li>
           <li className="check-box">
-            <input type="checkbox" />
+            <input type="checkbox" checked={barChecked} onChange={() => handleBarChange(barChecked)}/>
             <label>Bars</label>
           </li>
           <li className="check-box">
-            <input type="checkbox" />
+            <input type="checkbox" checked={equipChecked} onChange={() => handleEquipChange(equipChecked)}/>
             <label>Equipment</label>
           </li>
           <li className="check-box">
-            <input type="checkbox" />
+            <input type="checkbox" checked={retailChecked} onChange={() => handleRetailChange(retailChecked)}/>
             <label>Retail</label>
           </li>
           <li className="check-box">
-            <input type="checkbox" />
+            <input type="checkbox" checked={medicalChecked} onChange={() => handleMedicalChange(medicalChecked)}/>
             <label>Medical</label>
           </li>
           <li className="check-box">
-            <input type="checkbox" />
+            <input type="checkbox" checked={miscChecked} onChange={() => handleMiscChange(miscChecked)}/>
             <label>Misc.</label>
           </li>
         </ul>
